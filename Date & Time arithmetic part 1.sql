1b147e27e498692f7539f3c73aa0e06d280dd530
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2026-01-27 12:38:55 +0530

    Add files via upload

--- Date & Time arithmetic part 1.sql
+++ Date & Time arithmetic part 1.sql
@@ -59,3 +59,69 @@ WHERE start_time < NOW() - INTERVAL '10 days';
 SELECT task_name,start_time
 FROM project_tasks
 WHERE start_time < NOW() - INTERVAL '14 days';
+
+
+-- Current date & time 
+-- adding and subtracting days,months,years
+SELECT CURRENT_DATE;
+
+SELECT CURRENT_TIME;
+
+SELECT NOW();
+
+SELECT CURRENT_DATE + INTERVAL '5 days';
+
+SELECT CURRENT_DATE - INTERVAL '2 days';
+
+SELECT NOW() - INTERVAL '1 days'
+
+SELECT NOW() + INTERVAL '1 month';
+
+SELECT NOW() + INTERVAL '1 year';
+
+SELECT NOW();
+
+SELECT NOW() - INTERVAL '10 years';
+
+SELECT NOW() - INTERVAL '10 year';
+
+SELECT NOW() - INTERVAL '30 minutes';
+
+--Date difference
+SELECT CURRENT_DATE - DATE '2026-01-01';
+
+-- Using AGE()
+SELECT AGE(CURRENT_DATE, DATE '2000-05-15');
+
+--Finding my birthday :)
+SELECT AGE(CURRENT_DATE, DATE '2003-01-21') AS Sajin_Age;
+
+--EXTRACT Parts from date/time
+
+SELECT
+EXTRACT(YEAR FROM NOW()) AS year,
+EXTRACT(MONTH FROM NOW()) AS month,
+EXTRACT(MONTH FROM NOW()) AS day,
+EXTRACT(HOUR FROM NOW()) AS hour;
+
+
+SELECT NOW();
+
+SELECT EXTRACT (YEAR FROM NOW()) as year;
+
+--Truncate date/time
+
+SELECT DATE_TRUNC('day',NOW());
+
+SELECT DATE_TRUNC('month',NOW());
+SELECT DATE_TRUNC('year',NOW());
+
+SELECT NOW()::DATE;
+SELECT NOW()DATE;
+SELECT NOW();
+
+SELECT CURRENT_DATE::TIMESTAMP;
+SELECT CURRENT_DATE;
+
+SELECT NOW() AT TIME ZONE 'UTC';
+SELECT NOW() AT TIME ZONE 'Asia/Kolkata';
